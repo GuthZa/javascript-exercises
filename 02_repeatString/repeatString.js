e41fc1700a556671900895f36ebc718cd76a1ec8
Wisura <joao9.claro99@gmail.com>
2024-03-05 17:49:48 +0000

    Finish the first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
 
+    let strAux = "";
+    if (n < 0) return "ERROR";
+    for(let i = 0; i < n; i++)
+        strAux += str;
+    return strAux;
 };
 
 // Do not edit below this line
