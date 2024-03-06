06129fe1019e767434cac30b56577acdd27a08a5
Wisura <joao9.claro99@gmail.com>
2024-03-06 10:47:19 +0000

    Finish checking palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let size = Math.floor(string.length / 2);
+  for (let i = 0; i < size; i++) {
+    if (str[i] !== str[str.length - i - 1]) return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
