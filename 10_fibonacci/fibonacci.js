5cb718b8fa63d3ca26b7e11a018f94be5d662e74
Wisura <joao9.claro99@gmail.com>
2024-03-06 11:16:32 +0000

    Finish fibonacci calculator

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number == 0) return 0;
+  if (number == 1) return 1;
+  if (number < 0) return "OOPS";
+  //having Fa = F(n - 1)
+  //and    Fb = F(n - 2)
+  let Fa = 1;
+  let Fb = 0;
+  let Fn = 0;
+  for (let n = 2; n <= number; n++) {
+    Fn = Fa + Fb;
+    Fb = Fa;
+    Fa = Fn;
+  }
+  return Fn;
 };
 
 // Do not edit below this line
