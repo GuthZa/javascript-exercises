8cdf0474f82e2c1125833d7f99f92832a7e4bc9c
Wisura <joao9.claro99@gmail.com>
2024-03-06 11:23:08 +0000

    Finish exercise 11
    To be able to get properties inside an object

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let title = "title";
+  let titles = new Array();
+  books.forEach((element) => {
+    titles.push(element[title]);
+  });
+  return titles;
 };
 
 // Do not edit below this line
