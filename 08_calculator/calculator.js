eab26bdfadec40c06dfe35ad566d0d132cc9a53e
Wisura <joao9.claro99@gmail.com>
2024-03-11 11:32:50 +0000

    Update Calculator
    The functions were originally written as C, without the use of js built-in functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,37 +6,21 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function (numbers) {
-  let sum = 0;
-  for (const element of numbers) {
-    !isNaN(element) ? (sum += +element) : 0;
-  }
-  return sum;
-
-  //return array.reduce((total, current) => total + current, 0);
+const sum = function (array) {
+  return array.reduce((sum, num) => sum + num, 0);
 };
 
 const multiply = function (numbers) {
-  let sum = 1;
-  for (const element of numbers) {
-    !isNaN(element) ? (sum *= +element) : 0;
-  }
-  return sum;
-
-  //return array.reduce((product, current) => product * current);
+  return numbers.reduce((product, num) => product * num, 1);
 };
 
 const power = function (a, b) {
-  let sum = a;
-  for (let i = 1; i < b; i++) {
-    sum *= a;
-  }
-  return sum;
-
-  //return Math.pow(a,b);
+  return Math.pow(a, b);
 };
 
 const factorial = function (a) {
+
+  
   let sum = 1;
   while (a > 0) {
     sum *= a--;
